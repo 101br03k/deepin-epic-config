4cb240171911971b8f3226c91be49761d3c8262f
A3 <59868436+101br03k@users.noreply.github.com>
2020-06-21 11:00:50 +0200

    Update deepin_system-manager.sh

--- deepin_system-manager.sh
+++ deepin_system-manager.sh
@@ -1,2 +1,8 @@
+#install deepin system manager
 sudo pacman -S deepin-system-manager --noconfirm
+
+#install grub customizer
 sudo pacman -S grub-customizer --noconfirm
+
+#install sl
+sudo pacman -S sl --noconfirm
