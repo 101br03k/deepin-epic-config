9603a557691038311424e2c09ae5e5f9a7600992
101br03k <warmerdamm03@gmail.com>
2020-09-17 14:01:18 +0200

    added console icon

---
+++ CSGO-console/compile.sh
@@ -0,0 +1,2 @@
+g++ $(pkg-config --cflags --libs Qt5Widgets qtermwidget5) -fPIC -o CSGO-console CSGO-console.cpp
+./CSGO-console
--- csgo-terminal/compile.sh
+++
@@ -1,2 +0,0 @@
-g++ $(pkg-config --cflags --libs Qt5Widgets qtermwidget5) -fPIC -o csgo-console csgo-console.cpp
-./csgo-console
